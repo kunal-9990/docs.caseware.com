61a9bcdbccd9279ba3ee5e78160fce06a56980d6
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-08-28 14:59:36 -0400

    starter project styles

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,21 +1,23 @@
-<header>
-    <h1>LOGO</h1>
-    <div>
-        <ul>
-            <li>
-                <a href="#">EN</a>
-                <ul>
-                    <li>English</li>
-                    <li>French</li>
-                    <li>Spanish</li>
-                    <li>Dutch</li>
-                    <li>Chinese</li>
-                    <li>German</li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Filters</a>
-            </li>
-        </ul>
+<header class="header">
+    <div class="container">
+        <img src="img/caseware-cloud-logo.png" alt="">
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">EN</a>
+                    <ul>
+                        <li>English</li>
+                        <li>French</li>
+                        <li>Spanish</li>
+                        <li>Dutch</li>
+                        <li>Chinese</li>
+                        <li>German</li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Filters</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </header>
