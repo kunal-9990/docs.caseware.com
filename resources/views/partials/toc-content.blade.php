c999ff4e97ac8fb5045a77c37258b9fc6df6677c
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-09-04 11:57:58 -0400

    add sass-mq to project

--- resources/views/partials/toc-content.blade.php
+++ resources/views/partials/toc-content.blade.php
@@ -1,1 +1,6 @@
-<div>TOC CONTENT</div>
+<section class="toc-content">
+    <h1>What's new - Cloud 27.0</h1>
+    <h2>Improved engagement management</h2>
+    <p>Working Papers 2018 users can now view key engagement metrics directly on the Engagement page. Using Cloud, staff can monitor metrics - including document status, issues, deliverable dates, adjusting entries and more - across multiple engagements simultaneously.</p>
+    <img src="https://via.placeholder.com/1920x1080" alt="placeholder">
+</section>
