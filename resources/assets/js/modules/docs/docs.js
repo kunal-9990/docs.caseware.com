117ee5d9f417c6e24ba4f8967fc77430e208e3ac
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-09-11 11:15:36 -0400

    fix layout in xs/sm/md bps to swap classnames for layout width

--- resources/assets/js/modules/docs/docs.js
+++ resources/assets/js/modules/docs/docs.js
@@ -1,7 +1,9 @@
 const SUB_TOPIC_SCROLLSPY = require('./sub-topic-scrollspy');
 const TOC = require('./toc');
+const DOCS_CONTAINER = require('./docs-grid');
 
 module.exports = () => {
     SUB_TOPIC_SCROLLSPY();
     TOC();
+    DOCS_CONTAINER();
 };
