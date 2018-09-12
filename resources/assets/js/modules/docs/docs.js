d834be3206ecabdc72371e3ed5809a583684bf8b
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-09-12 09:41:31 -0400

    rename component

--- resources/assets/js/modules/docs/docs.js
+++ resources/assets/js/modules/docs/docs.js
@@ -1,6 +1,6 @@
 const SUB_TOPIC_SCROLLSPY = require('./sub-topic-scrollspy');
 const TOC = require('./toc');
-const DOCS_CONTAINER = require('./docs-grid');
+const DOCS_CONTAINER = require('./grid');
 
 module.exports = () => {
     SUB_TOPIC_SCROLLSPY();
