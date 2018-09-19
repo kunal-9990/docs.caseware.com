fd533523167b353de4feeb47a81553afbd230e68
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-09-19 10:47:22 -0400

    clean up

--- resources/views/partials/image-modal.blade.php
+++ resources/views/partials/image-modal.blade.php
@@ -1,13 +1,5 @@
 <div class="image-modal">
     <img class="image-modal__img">
     <div class="image-modal__caption"></div>
-    {{-- <span class="image-modal__close">&times;</span> --}}
     <span class="glyphicon glyphicon-remove image-modal__close" aria-hidden="true"></span>
 </div>
-
-{{-- <div id="myimgModal" class="imgmodal">
-    <img id="modalimg">
-    <span class="closemodal">×</span>
-    <div id="caption">
-    </div>
-</div> --}}
