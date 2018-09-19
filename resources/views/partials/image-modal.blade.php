04fd3e8578bf546c16a844327482dc0af59b291c
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-09-19 13:59:35 -0400

    remove figcaption texxt, not even sure how that got there :S

--- resources/views/partials/image-modal.blade.php
+++ resources/views/partials/image-modal.blade.php
@@ -1,7 +1,7 @@
 <div class="image-modal">
     <figure>
         <img class="image-modal__img" />
-        <figcaption class="image-modal__caption">Tree frog by David Clode on Unsplash</figcaption>
+        <figcaption class="image-modal__caption"></figcaption>
     </figure>
     <span class="glyphicon glyphicon-remove image-modal__close" aria-hidden="true"></span>
 </div>
