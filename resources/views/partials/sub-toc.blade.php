b74b5ea0934f7a61f61966c3fff69a84b98fa28a
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-09-24 16:50:35 -0400

    cleanup

--- resources/views/partials/sub-toc.blade.php
+++ resources/views/partials/sub-toc.blade.php
@@ -1,6 +1,4 @@
-{{-- https://getbootstrap.com/docs/3.3/javascript/#affix --}}
-{{-- <section class="toc-subtopics" data-spy="affix" data-offset-top="86"> --}}
-<section class="toc-subtopics" id="basic-waypoint">
+<section class="toc-subtopics">
     <h5>in this article</h5>
     <ul class="nav">
         <li><a href="#view-cost-billing-summary">Viewing cost and billing summaries</a></li>
