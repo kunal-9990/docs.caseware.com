fe0d9ca75af35d7c26c3297e8fe66ceae8101d8c
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-09-25 14:56:16 -0400

    cleanup

--- resources/views/partials/sub-toc.blade.php
+++ resources/views/partials/sub-toc.blade.php
@@ -1,5 +1,5 @@
 <section class="toc-subtopics">
-    <h5>in this article</h5>
+    <h5>In this article</h5>
     <ul class="nav">
         <li><a href="#view-cost-billing-summary">Viewing cost and billing summaries</a></li>
         <li><a href="#generate-time-report">Generating a Time and Expense report</a></li>
