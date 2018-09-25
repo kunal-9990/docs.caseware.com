01ca0c9f370c8c3e60332cd3af204ba98f68fe54
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-09-25 10:57:52 -0400

    cleanup

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,49 +4,25 @@
             <div class="header__nav-wrap">
                 <img src="/img/caseware-cloud-logo.png" alt="CaseWare Cloud logo">
                 <ul class="header__nav">
-                        <li>
-                            <a href="google.com" class="header__lang-picker--js">English <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>
-                            <ul class="header__lang-picker">
-                                <li>English</li>
-                                <li>French</li>
-                                <li>Spanish</li>
-                                <li>Dutch</li>
-                                <li>Chinese</li>
-                                <li>German</li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">Filters</a>
-                        </li>
-                    </ul>
+                    <li>
+                        <a href="google.com" class="header__lang-picker--js">English <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>
+                        <ul class="header__lang-picker">
+                            <li>English</li>
+                            <li>French</li>
+                            <li>Spanish</li>
+                            <li>Dutch</li>
+                            <li>Chinese</li>
+                            <li>German</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Filters</a>
+                    </li>
+                </ul>
             </div>
-            {{-- <img src="/img/caseware-cloud-logo.png" alt="CaseWare Cloud logo"> --}}
             <div class="header__input-search-wrapper">
                 <input type="text" placeholder="Search">
             </div>
         </div>
-        {{-- <div>
-            <ul class="header__nav">
-                <li>
-                    <a href="google.com" class="header__lang-picker--js">EN</a>
-                    <ul class="header__lang-picker">
-                        <li>English</li>
-                        <li>French</li>
-                        <li>Spanish</li>
-                        <li>Dutch</li>
-                        <li>Chinese</li>
-                        <li>German</li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#">Filters</a>
-                </li>
-            </ul>
-        </div> --}}
     </div>
 </header>
-
-{{-- import for filters popup with desktop style wrapper --}}
-{{-- <div class="content-filter--desktop-only">
-    @include('partials.content-filter')
-</div> --}}
