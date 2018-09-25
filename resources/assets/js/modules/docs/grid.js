1a9783132aaa780192e8400a9ac8105ca3631a9c
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-09-25 10:55:31 -0400

    rework nav layout

--- resources/assets/js/modules/docs/grid.js
+++ resources/assets/js/modules/docs/grid.js
@@ -2,14 +2,19 @@ module.exports = () => {
     // for xs/sm/md bps we use container-fluid bootstrap class
 
     const DOCS_CONTAINER = document.querySelector('div.container.documentation');
+    const HEADER_CONTAINER = document.querySelector('div.container.header__container');
 
     function setGridClass() {
         if (window.innerWidth <= 1199) {
             DOCS_CONTAINER.classList.remove('container');
             DOCS_CONTAINER.classList.add('container-fluid');
+            HEADER_CONTAINER.classList.remove('container');
+            HEADER_CONTAINER.classList.add('container-fluid');
         } else {
             DOCS_CONTAINER.classList.remove('container-fluid');
             DOCS_CONTAINER.classList.add('container');
+            HEADER_CONTAINER.classList.remove('container-fluid');
+            HEADER_CONTAINER.classList.add('container');
         }
     }
     setGridClass();
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,31 @@
 <header class="header">
-    <div class="container">
+    <div class="container header__container">
         <div class="header__search-wrap">
-            <img src="/img/caseware-cloud-logo.png" alt="CaseWare Cloud logo">
+            <div class="header__nav-wrap">
+                <img src="/img/caseware-cloud-logo.png" alt="CaseWare Cloud logo">
+                <ul class="header__nav">
+                        <li>
+                            <a href="google.com" class="header__lang-picker--js">English <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>
+                            <ul class="header__lang-picker">
+                                <li>English</li>
+                                <li>French</li>
+                                <li>Spanish</li>
+                                <li>Dutch</li>
+                                <li>Chinese</li>
+                                <li>German</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Filters</a>
+                        </li>
+                    </ul>
+            </div>
+            {{-- <img src="/img/caseware-cloud-logo.png" alt="CaseWare Cloud logo"> --}}
             <div class="header__input-search-wrapper">
                 <input type="text" placeholder="Search">
             </div>
         </div>
-        <div>
+        {{-- <div>
             <ul class="header__nav">
                 <li>
                     <a href="google.com" class="header__lang-picker--js">EN</a>
@@ -23,7 +42,7 @@
                     <a href="#">Filters</a>
                 </li>
             </ul>
-        </div>
+        </div> --}}
     </div>
 </header>
 
