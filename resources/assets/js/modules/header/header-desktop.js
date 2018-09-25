e61d1e702920ceb3e61d9a5241684dffb8caddc5
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-09-25 13:52:01 -0400

    language filter. functionality todo

--- resources/assets/js/modules/header/header-desktop.js
+++ resources/assets/js/modules/header/header-desktop.js
@@ -1,16 +1,6 @@
 module.exports = () => {
-    const LANG_DROPDOWN = document.querySelector('.header__lang-picker--js');
-    const HEADER_NAV = document.querySelector('.header__nav');
     const DESKTOP_SEARCH = document.querySelector('.header__input-search-wrapper');
     const BODY = document.querySelector('body');
-
-    function toggleLangDropdown(event) {
-        const langToggle = HEADER_NAV.classList;
-        event.preventDefault();
-        langToggle.contains('lang-list--is-open')
-            ? langToggle.remove('lang-list--is-open')
-            : langToggle.add('lang-list--is-open');
-    }
 
     function toggledesktopsearch() {
         if (!BODY.classList.contains('desktop-search--is-open')) {
@@ -18,7 +8,6 @@ module.exports = () => {
         }
     }
 
-    LANG_DROPDOWN.addEventListener('click', () => toggleLangDropdown());
     DESKTOP_SEARCH.addEventListener('click', () => toggledesktopsearch());
 
     window.addEventListener('click', (event) => {
