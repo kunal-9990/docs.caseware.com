f9700f7cb27d13124138ea426233e111d9c50c32
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-10-01 09:30:48 -0400

    add footer module

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="footer">
+    {{-- <div class="container">
+        <div class="row">
+            <div class="col-sm-4"><a href="https://www.caseware.com"><img src="/img/CaseWare_logo_RGB_horz_White.png" class="footerlogo" width="280"></a></div>
+            <div class="col-sm-4"><p class="">Copyright © 2017 CaseWare International</p></div>
+            <div class="col-sm-4 footer__social-media">
+                    <a href="https://www.linkedin.com/groups/1935174/profile"><i class="fab fa-linkedin"></i></a>
+                    <a href="http://www.facebook.com/pages/CaseWare-International-Inc/54418196590?ref=ts"><i class="fab fa-facebook-square"></i></a>
+                    <a href="https://twitter.com/CaseWare"><i class="fab fa-twitter-square"></i></a>
+                    <a href="http://www.youtube.com/user/casewarevids?feature=results_main"><i class="fab fa-youtube-square"></i></a>
+                    <a href="https://www.caseware.com/caseware-rss"><i class="fas fa-rss-square"></i></a>
+            </div>
+        </div>
+    </div> --}}
+    <div class="container">
+        <div class="footer__flex-container">
+        <a href="https://www.caseware.com"><img class="footer__cw-logo" src="/img/CaseWare_logo_RGB_horz_White.png" class="footerlogo"></a>
+        <p class="">Copyright © 2017 CaseWare International</p>
+        <div class="footer__social-media">
+            <a href="https://www.linkedin.com/groups/1935174/profile"><i class="fab fa-linkedin"></i></a>
+            <a href="http://www.facebook.com/pages/CaseWare-International-Inc/54418196590?ref=ts"><i class="fab fa-facebook-square"></i></a>
+            <a href="https://twitter.com/CaseWare"><i class="fab fa-twitter-square"></i></a>
+            <a href="http://www.youtube.com/user/casewarevids?feature=results_main"><i class="fab fa-youtube-square"></i></a>
+            <a href="https://www.caseware.com/caseware-rss"><i class="fas fa-rss-square"></i></a>
+        </div>
+        </div>
+    </div>
+</footer>
