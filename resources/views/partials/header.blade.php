f65045d86b1fe4d7f43904c987070673b14d0990
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-10-17 09:52:41 -0400

    remove autocomplete from search

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -18,7 +18,7 @@ use Illuminate\Http\Request;
                 $searchURL = route('search1', [$product, $version, $lang]);
                 @endphp
                 <form method="GET" action="{{$searchURL}}">
-                    <input type="text" name="search" placeholder="Search">
+                    <input type="text" name="search" placeholder="Search" autocomplete="off">
                 </form>
             </div>
         </div>
