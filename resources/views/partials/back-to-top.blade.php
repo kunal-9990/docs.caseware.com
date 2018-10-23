c79e3117f2fdc460864b6f2bd15923885b11ee72
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-10-23 09:54:00 -0400

    change back to top btn

--- resources/views/partials/back-to-top.blade.php
+++ resources/views/partials/back-to-top.blade.php
@@ -1,5 +1,3 @@
 <div class="back-to-top">
-    <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
-    <span>Back to</span>
-    <span>top</span>
+    <span class="glyphicon glyphicon-circle-arrow-up"></span>
 </div>
