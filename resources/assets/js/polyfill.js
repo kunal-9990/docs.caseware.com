e57c69b379e118e6391a1ba223e77afcfdc6d30c
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-10-30 10:45:55 -0400

    add polyfill for .closest IE support

--- resources/assets/js/polyfill.js
+++ resources/assets/js/polyfill.js
@@ -2,3 +2,20 @@
 if (window.NodeList && !NodeList.prototype.forEach) {
     NodeList.prototype.forEach = Array.prototype.forEach;
 }
+
+// Element.closest() (IE support)
+if (!Element.prototype.matches)
+    Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                                Element.prototype.webkitMatchesSelector;
+
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+        var el = this;
+        if (!document.documentElement.contains(el)) return null;
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1); 
+        return null;
+    };
+}
