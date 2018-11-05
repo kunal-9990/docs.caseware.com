9d51e42fdce20fd68cbd88a0302cd0023c0f090f
Matthew Chiang <matthew.chiang@caseware.com>
2018-11-05 09:37:25 -0500

    fixing redirect path

--- resources/assets/js/modules/docs/redirects.js
+++ resources/assets/js/modules/docs/redirects.js
@@ -15,6 +15,7 @@ module.exports = () => {
     // version = routeParams[4]; 
     // lang = routeParams[5]; 
 
+    year = routeParams[3];
     product = routeParams[4];
     version = routeParams[5]; 
     lang = routeParams[6]; 
@@ -26,7 +27,7 @@ module.exports = () => {
 
     $.ajax({
       type: "GET",
-      url: "/documentation_files/" + product + "/" + version + "/csh_redirect.xml",
+      url: "/documentation_files/" +year + "/" + product + "/" + version + "/csh_redirect.xml",
       dataType: "xml",
       success: function (xml) {
         url = window.location.href.split("?")[0];
@@ -35,14 +36,14 @@ module.exports = () => {
                       console.log("found");
                       // uncomment when live
                       // var linkPrefix = "/" + product + "/" + version + "/" + lang;
-                      var linkPrefix = "/docsmk2/" + product + "/" + version + "/" + lang;
+                      var linkPrefix = "/" + year + "/" + product + "/" + version + "/" + lang;
                       window.location.replace(linkPrefix + $(this).find("Redirectto").text());
             return false;
           }
         });
       } 
     });
-
+ 
     $("#"+accordianID).addClass("in");
     $("body, html").animate({
       scrollTop: $("#" + accordianID).offset().top - 100 
