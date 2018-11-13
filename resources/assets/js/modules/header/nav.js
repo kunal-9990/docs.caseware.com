d9f46fe55d102450397cc809fe3328c0405e4e25
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-11-13 13:35:29 -0500

    finish filters

--- resources/assets/js/modules/header/nav.js
+++ resources/assets/js/modules/header/nav.js
@@ -2,15 +2,15 @@ module.exports = () => {
     // const filter = require('../filter');
     const BODY = document.querySelector('body');
     const FILTER_DROPDOWN = document.querySelector('.filters__dropdown');
-    const FILTER_ITEMS = document.querySelectorAll('.expanded-filters .filters__item');
+    const FILTER_ITEMS = document.querySelectorAll('span.slider.round');
 
     FILTER_DROPDOWN.addEventListener('click', () => {
         BODY.classList.toggle('filter-dropdown-is-expanded');
     });
 
     FILTER_ITEMS.forEach((item) => {
-        item.addEventListener('click', () => {
-            item.classList.toggle('active-filter');
+        item.addEventListener('click', (event) => {
+            event.target.closest('.switch-wrap').classList.toggle('is-active');
         });
     });
 };
