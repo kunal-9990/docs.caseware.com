57adfa72901626c3022f924ad8b8d3adb9f1eb52
Matthew Chiang <matthew.chiang@caseware.com>
2018-11-14 11:43:24 -0500

    adding json ld snippet to meta

--- resources/views/partials/filter-msg.blade.php
+++ resources/views/partials/filter-msg.blade.php
@@ -6,5 +6,5 @@ if($exclusiveTo == 'Wp'){
 @endphp
 
 <div class="filtermsg col-sm-9">
-    This content is exclusively related to {{$exclusiveTo}} and has been filtered out. Select the <span class="glyphicon glyphicon-filter"></span> icon in the top navigation to modify your filter settings.
+    This content is exclusively related to {{$exclusiveTo}} and has been filtered out. Select the Filter dropdown in the top navigation to modify your filter settings.
 </div>
