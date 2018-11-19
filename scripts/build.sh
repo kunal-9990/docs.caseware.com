50952007d8d5f24c7dde31da1d9715ec3759f9ef
Matthew Chiang <matthew.chiang@caseware.com>
2018-11-19 18:22:12 -0500

    build script improved

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,75 @@
+# Adding a new help version:
+#
+# ----------------------------
+# /docsmk2/
+# │
+# └───/tmp/
+# │   │  
+# │   └───/30/
+# │       │   
+# │       └─── /Content/
+# │       │    Online Output.fltoc
+# │       │    csh_redirect.xml
+#
+# ----------------------------
+#
+# run npm build and supply the year, product, and version as parameters.
+# for example:
+# npm run build 2018 webapps 30
+
+
+
+
+sudo chmod -R 777 .
+
+echo 'Moving stuff around...'
+
+sudo mkdir -p public/documentation_files/$1/$2/$3
+sudo cp -R tmp/$3/* public/documentation_files/$1/$2/$3
 cd public/documentation_files/$1/$2/$3
-# prefix="/documentation_files/$1/$2/$3/Content/Resources/"
-# prefix="$prefix"
-find . -type f -print0 | xargs -0 sed -i 's/src="\/...\/...\/Resources/src="\/documentation_files\/2018\/webapps\/29\/Content\/Resources/g'
-find . -type f -print0 | xargs -0 sed -i 's/src="..\/..\/Resources/src="\/documentation_files\/2018\/webapps\/29\/Content\/Resources/g'
-find . -type f -print0 | xargs -0 sed -i 's/src="..\/..\/..\/Resources/src="\/documentation_files\/2018\/webapps\/29\/Content\/Resources/g'
-find . -type f -print0 | xargs -0 sed -i 's/src="\/Resources/src="\/documentation_files\/2018\/webapps\/29\/Content\/Resources/g'
+
+sudo chmod -R 777 .
+
+
+echo 'Renaming stuff...'
+
 find -name "*.fltoc" -print0 | xargs -0 sed -i 's/\/Content//g'
 
+sudo mv Online\ Output.fltoc OnlineOutput.xml
 
+
+prefix="\/documentation_files\/$1\/$2\/$3\/Content\/Resources\/"
+prefix="$prefix"
+
+echo 'Updating img src paths...'
+
+find . -type f -print0 | xargs -0 sed -i 's/src="\/...\/...\/Resources/src="'"$prefix"'/g'
+find . -type f -print0 | xargs -0 sed -i 's/src="..\/..\/Resources/src="'"$prefix"'/g'
+find . -type f -print0 | xargs -0 sed -i 's/src="..\/..\/..\/Resources/src="'"$prefix"'/g'
+find . -type f -print0 | xargs -0 sed -i 's/src="\/Resources/src="'"$prefix"'/g'
+
+
+cd ../../../../..
+
+
+echo 'Copying Data folders into place...'
+
+sudo mkdir -p public/$1/$2/$3/en
+sudo cp -R tmp/$3/Content/Data public/$1/$2/$3/en
+cd public/$1/$2/$3
+
+
+sudo cp -R en fr
+sudo cp -R en es
+sudo cp -R en nl
+sudo cp -R en cn
+sudo cp -R en de
+
+cd ../../../..
+
+echo 'Setting File permissions...'
+
+sudo find . ! -name '*.sh' -type f -exec chmod 644 {} \;    
+sudo find . ! -name '*.sh' -type d -exec chmod 755 {} \;
+
+echo 'Done.'
