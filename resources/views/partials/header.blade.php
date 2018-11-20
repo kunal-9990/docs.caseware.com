4a627675e47169e12468c9ab218f7d4ce6280569
Matthew Chiang <matthew.chiang@caseware.com>
2018-11-20 11:23:24 -0500

    linking header logo to index page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,20 @@
+@php 
+$year = Route::current()->parameters()["year"];
+$product = Route::current()->parameters()["product"];
+$version = Route::current()->parameters()["version"];
+$lang = Route::current()->parameters()["lang"];
+$searchURL = route('search', [$year, $product, $version, $lang]);
+$indexURL = route('category', [$year, $product, $version, $lang, 'webapps']);
+@endphp
 <header class="header">
     <div class="container header__container">
         {{-- <div class="row"> --}}
         <div class="header__search-wrap">
             <div class="header__nav-wrap">
-                <a href="#"><img src="/img/caseware-cloud-logo.png" alt="CaseWare Cloud logo"></a>
+                <a href="{{$indexURL}}"><img src="/img/caseware-cloud-logo.png" alt="CaseWare Cloud logo"></a>
                 @include('partials.nav')
             </div>
             <div class="header__input-search-wrapper">
-                @php
-                $year = Route::current()->parameters()["year"];
-                $product = Route::current()->parameters()["product"];
-                $version = Route::current()->parameters()["version"];
-                $lang = Route::current()->parameters()["lang"];
-                $searchURL = route('search', [$year, $product, $version, $lang]);
-                @endphp
                 <form method="GET" action="{{$searchURL}}">
                     <input type="text" name="search" placeholder="Search" autocomplete="off">
                 </form>
