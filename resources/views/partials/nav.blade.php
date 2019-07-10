61ced5ea454430a9986c477bfcc14c2cf35ce4a3
Alex Tarapacky <alex.tarapacky@caseware.com>
2019-07-10 10:56:09 -0400

    set correct links in nav when the current link is a properly translated file

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -19,18 +19,24 @@
     @php
     $languages = array("en"=>"EN", "fr"=>"FR", "es"=>"ES", "nl"=>"NL","cn"=>"CN", "de"=>"DE");
     $segments = Request::segments();
-
+    $currentLanguage = $languages[Request::segments()[3]];
     foreach ($languages as $key => $language) {
         $segments[3] = $key;
         ${$key . 'Link'} = '/' . implode('/', $segments);
-        if (isset($exclusiveTo) && $exclusiveTo != '') {
-            ${$key . 'TranslatedFile'} = env('PATH_TO_PUBLIC') . 'documentation_files/' . implode('/', array_slice($segments, 0, 3)) . '/Content/en/Resources/TranslatedDocs/' . strtoupper($segments[3]) . '/' . $segments[6];
-            if (file_exists(${$key . 'TranslatedFile'})) {
+        ${$key . 'TranslatedFile'} = env('PATH_TO_PUBLIC') . 'documentation_files/' . implode('/', array_slice($segments, 0, 3)) . '/Content/en/Resources/TranslatedDocs/' . strtoupper($segments[3]) . '/' . ($segments[5] != 'TranslatedDocs' ? $segments[6] : $segments[7]);
+        if ($segments[5] != 'TranslatedDocs') {
+            if (isset($exclusiveTo) && $exclusiveTo != '' && file_exists(${$key . 'TranslatedFile'})) {
                 ${$key . 'Link'} = '/' . implode('/', array_slice($segments, 0, 3)) . '/en/Resources/TranslatedDocs/' . strtoupper($key) . '/' . $segments[6];
+            }
+        } else {
+            $currentLanguage = $languages[strtolower(Request::segments()[6])];
+            if (file_exists(${$key . 'TranslatedFile'})) {
+                ${$key . 'Link'} = '/' . implode('/', array_slice($segments, 0, 3)) . '/en/Resources/TranslatedDocs/' . strtoupper($key) . '/' . $segments[7];
+            } else {
+                ${$key . 'Link'} = '/' . implode('/', array_slice($segments, 0, 3)) . '/' . $key . '/Setup/Licenses/' . $segments[7];
             }
         }
     }
-    $currentLanguage = $languages[Request::segments()[3]];
     @endphp
     <a href="#"><i class="fas fa-globe-americas"></i> {{$currentLanguage}} <i class="fas fa-angle-down"></i></a>
     <div class="dropdown-content">
