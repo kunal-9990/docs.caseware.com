0f2f3b01d564ac423e0fe174fe8293369b62f4be
Alex Tarapacky <alex.tarapacky@caseware.com>
2020-04-06 12:13:47 -0400

    footer styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,16 +1,32 @@
+<section class="newsletter">
+    <div class="newsletter__container container">
+        <h3>TODO - Newsletter Signup</h3>
+    </div>
+</section>
 <footer class="footer">
     <div class="container">
-        <div class="footer__flex-container">
-        <a href="https://www.caseware.com"><img class="footer__cw-logo" src="/img/CaseWare_logo_RGB_horz_White.png" class="footerlogo" alt="CaseWare logo"></a>
-        <div class="footer__social-media">
-            <a href="https://www.linkedin.com/groups/1935174/profile"><i class="fab fa-linkedin"></i></a>
-            <a href="http://www.facebook.com/pages/CaseWare-International-Inc/54418196590?ref=ts"><i class="fab fa-facebook-square"></i></a>
-            <a href="https://twitter.com/CaseWare"><i class="fab fa-twitter-square"></i></a>
-            <a href="http://www.youtube.com/user/casewarevids?feature=results_main"><i class="fab fa-youtube-square"></i></a>
+        <div class="row">
+            <div class="col col--sm-full">        
+                <a href="https://www.caseware.com">
+                    <img class="footer__logo" src="/img/CaseWare_logo_RGB_horz_White.png" class="footerlogo" alt="CaseWare logo" />>
+                </a>
+            </div>
+            <div class="col col--sm-full footer__social-media">
+                <a href="https://www.linkedin.com/groups/1935174/profile"><i class="fab fa-linkedin"></i></a>
+                <a href="http://www.facebook.com/pages/CaseWare-International-Inc/54418196590?ref=ts"><i class="fab fa-facebook-square"></i></a>
+                <a href="https://twitter.com/CaseWare"><i class="fab fa-twitter-square"></i></a>
+                <a href="http://www.youtube.com/user/casewarevids?feature=results_main"><i class="fab fa-youtube-square"></i></a>
+            </div>
         </div>
+        <div class="row">
+            <div class="col"><h4>Products</h4></div>
+            <div class="col"><h4>About</h4></div>
+            <div class="col"><h4>Resources</h4></div>
+            <div class="col"><h4>Support</h4></div>
+            <div class="col"><h4>Contact</h4></div>
         </div>
     </div>
     <div class="footer__copyright">
-        <p class="">Copyright © 2020 CaseWare International</p>
+        <p class="">Copyright &copy; <?=date('Y');?> CaseWare International</p>
     </div>
 </footer>
