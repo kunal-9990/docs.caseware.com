4f95827900ae219e1366457da4e8e6047f520f23
Alex Tarapacky <alex.tarapacky@caseware.com>
2020-04-08 15:59:01 -0400

    test

--- resources/assets/js/modules/cookie-consent.js
+++ resources/assets/js/modules/cookie-consent.js
@@ -4,6 +4,7 @@ module.exports = () => {
   const cookieNotification = document.querySelector('.cookie');
 
   cookieBtn.addEventListener('click', () => {
+    console.log("Consent button click");
     setCookie('cookie-consent', true)
     cookieNotification.style.display = "none";
   });
