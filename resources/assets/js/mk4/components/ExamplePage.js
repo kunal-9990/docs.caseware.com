c8c3496ec6557c06a9fe61a3ad4a7450a9622698
Alex Tarapacky <alex.tarapacky@caseware.com>
2020-04-15 14:56:52 -0400

    natural react with imports

--- resources/assets/js/mk4/components/ExamplePage.js
+++ resources/assets/js/mk4/components/ExamplePage.js
@@ -1,29 +1,29 @@
 
-// import React from 'react';
+import React from 'react';
 
-// class ExamplePage extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {isToggleOn: true};
-//     }
+class ExamplePage extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
+    }
   
-//     handleClick() {
-//       this.setState(prevState => ({
-//         isToggleOn: !prevState.isToggleOn
-//       }));
-//       console.log("click!")
-//     }
+    handleClick() {
+      this.setState(prevState => ({
+        isToggleOn: !prevState.isToggleOn
+      }));
+      console.log("click!")
+    }
   
-//     render() {
-//       return (
-//         <div className="home-elements-2">
-//             <h2>It is {new Date().toLocaleTimeString()}.</h2>
-//             <button onClick={() => this.handleClick()}>
-//                 {this.state.isToggleOn ? 'ON' : 'OFF'}
-//             </button>
-//         </div>
-//       );
-//     }
-//   }
+    render() {
+      return (
+        <div className="home-elements-2">
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            <button onClick={() => this.handleClick()}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        </div>
+      );
+    }
+  }
   
-//   export default ExamplePage
+  export default ExamplePage
