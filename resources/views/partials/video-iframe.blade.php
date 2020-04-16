d413bea50bb822fe0020cd463c4d9abe7c5ea2f3
Matthew Chiang <matthew.chiang@caseware.com>
2020-04-16 19:18:54 -0400

    cms integration

--- resources/views/partials/video-iframe.blade.php
+++ resources/views/partials/video-iframe.blade.php
@@ -1,1 +1,1 @@
-<iframe @if($featured_video) src="{{$featured_video}} @endif class="yt-video-iframe" src="" frameborder="0" allowfullscreen></iframe>
+<iframe class="yt-video-iframe" src="" frameborder="0" allowfullscreen></iframe>
