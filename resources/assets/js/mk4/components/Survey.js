1906c658bd6cac3d6cfed8d256c6f4edfb366b5f
Alex Tarapacky <alex.tarapacky@caseware.com>
2020-04-17 16:59:30 -0400

    survey slide in partially working

--- resources/assets/js/mk4/components/Survey.js
+++ resources/assets/js/mk4/components/Survey.js
@@ -1,26 +1,41 @@
-
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import VisibilitySensor from 'react-visibility-sensor' 
+import Slide from 'react-reveal/Slide';
 
 class Survey extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-      };
-    }
   
-    render() {
-      return (
-        <div className="survey">
-          <h1>Survey - popup on scrollTo footer</h1>
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: false
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange (isVisible) {
+    console.log('Survey is now %s', isVisible ? 'visible' : 'hidden');
+    this.setState({ isVisible });
+  }
+
+  render() {
+    return (
+      <VisibilitySensor 
+        partialVisibility 
+        onChange={this.onChange}
+      >
+        <div className="survey-wrapper">
+          <Slide right when={this.state.isVisible}>
+            <div className="survey">
+              <div className="survey__header">Fill me out</div>
+              <div className="survey__content">
+                  hsadlfjhkljdash
+              </div>
+            </div>
+          </Slide>
         </div>
-      );
-    }
+      </VisibilitySensor>
+    );
+  }
 }
 
-// Feature.propTypes = {
-//     votes: PropTypes.number.isRequired,
-//     showVoter: PropTypes.bool,
-//   }
-  
-export default Survey
+export default Survey;
