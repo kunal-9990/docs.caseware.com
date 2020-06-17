f9c1c50ac66a08d89ffcba51c8cdeb1e8f432e35
Alex Tarapacky <alex.tarapacky@caseware.com>
2020-06-17 15:17:01 -0400

    breadcrumb fix

--- resources/assets/js/mk4/components/pages/blog/BlogMetaData.js
+++ resources/assets/js/mk4/components/pages/blog/BlogMetaData.js
@@ -15,7 +15,7 @@ const BlogMetaData = ({ postTitle, author, date, readTime, tags, allTags, catego
   return (
     <div className="metadata">
       <div className="metadata__breadcrumb">
-        <a href="/">Home</a>&nsbp;&nsbp;/&nsbp;&nsbp;<a href="/blog">Blog</a>&nsbp;&nsbp;/&nsbp;&nsbp;{ postTitle }
+        <a href="/">Home</a><span>/</span><a href="/blog">Blog</a><span>/</span>{ postTitle }
       </div>
       <div>
         <span>Author</span>{ author }
