8df5d0fbb21c21bd6a1b5fa60c84ef67b0b72d85
Alex Tarapacky <alex.tarapacky@caseware.com>
2020-06-17 11:18:22 -0400

    faq with independent accordion component

--- resources/assets/js/mk4/components/pages/secondary/FAQ.js
+++ resources/assets/js/mk4/components/pages/secondary/FAQ.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import Accordion from '../../Accordion'
 
 class FAQ extends Component {
+
   constructor() {
     super();
     this.state = {
@@ -8,10 +10,27 @@ class FAQ extends Component {
     }
   }
 
-  render() {
+
+
+  render () {
     console.log(this.props)
     return (
-<h1>FAQ</h1>
+      <div className="accordion">
+        {Object.keys(this.props).map(key => ( 
+          this.props[key].section && (
+            <div className="accordion__faq">
+              <h2>{ this.props[key].section['section_title'] }</h2>
+              { this.props[key].section['questions'].map((q, i) => (
+                <Accordion 
+                  id={i}
+                  title={q.question}
+                  content={q.answer}
+                />
+              )) }
+            </div>
+          )
+        ))}
+      </div>
     )
   }
 }
