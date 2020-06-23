a29b2be5f5c6d17dd244ea02e9640b21fef8f990
Alex Tarapacky <alex.tarapacky@caseware.com>
2020-06-23 16:43:54 -0400

    updates

--- resources/assets/js/mk4/components/pages/secondary/VideoGridItem.js
+++ resources/assets/js/mk4/components/pages/secondary/VideoGridItem.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react'
 import Fade from 'react-reveal/Fade'
-import Modal from 'react-modal';
+import Modal from 'react-modal'
+import YouTube from 'react-youtube'
 Modal.setAppElement('#main');
 
-const VideoLightbox = ({ item, handleCloseModal }) => (
-  <div className="video-lightbox">
-    <h1 dangerouslySetInnerHTML={{__html: item.title}}></h1>
-    <button onClick={handleCloseModal}>X</button>
-  </div>
-)
+
+const YouTubeOptions = {
+  // height: '390',
+  // width: '640',
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 0
+  }
+}
+
+const VideoLightbox = ({ item, handleCloseModal }) => {
+  console.log(item)
+
+  return(
+    <div className="video-lightbox__container">
+      <div className="video-lightbox__header">
+        <button 
+          onClick={handleCloseModal}
+          name="close"
+          aria-label="close"
+        >&times;</button>
+      </div>
+      <div className="video-lightbox__content">
+        <YouTube videoId={item.acf.youtube_id} opts={YouTubeOptions} onReady={this._onReady} />
+        <div className="video-lightbox__details">
+          <h2 dangerouslySetInnerHTML={{__html: item.title}}></h2>
+          <div dangerouslySetInnerHTML={{__html: item.acf.description}} />
+        </div>
+      </div>
+    </div>
+  )
+  }
 
 class VideoGridItem extends Component {
 
@@ -63,6 +90,9 @@ class VideoGridItem extends Component {
           onRequestClose={this.handleCloseModal}
           contentLabel="Video Lightbox"
           id={item.slug}
+          portalClassName="video-lightbox"
+          overlayClassName="video-lightbox__overlay"
+          className="video-lightbox__wrapper"
         >
           <VideoLightbox item={item} handleCloseModal={this.handleCloseModal} />
         </Modal>
@@ -72,3 +102,61 @@ class VideoGridItem extends Component {
 }
 
 export default VideoGridItem
+
+
+
+// class VideoLightbox extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       videos: [],
+//       loading: true
+//     }
+//   }
+//   componentDidMount() {
+//     var that = this;
+//     var API_key = "RlosKtcCJVii9rsOvIB5gg";
+//     var channelID = "UCRlosKtcCJVii9rsOvIB5gg";
+//     var maxResults = 10;
+//     var url =
+//       "https://www.googleapis.com/youtube/v3/search?key=" +
+//       API_key +
+//       "&channelId=" +
+//       channelID +
+//       "&part=snippet,id&order=date&maxResults=" +
+//       maxResults;
+//     fetch(url)
+//       .then(function(response) {
+//         if (response.status >= 400) {
+//           throw new Error("Bad response from server");
+//         }
+//         return response.json();
+//       })
+//       .then(function(data) {
+//         that.setState({ videos: data.items, loading: false });
+//       })
+//       .catch(error => {
+//         console.error(error);
+//       });
+//   }
+//   render() {
+//     if (this.state.loading) {
+//       return null;
+//     }
+//     return (
+//       <div className="App">
+//         <YouTube
+//           videoId={this.state.videos[1].id.videoId}
+//           opts={{
+//             height: "390",
+//             width: "640",
+//             playerVars: {
+//               autoplay: 1
+//             }
+//           }}
+//           onReady={this._onReady}
+//         />
+//       </div>
+//     );
+//   }
+// }
