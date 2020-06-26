de7884218e64929550b498884c9279d287eb56b2
Alex Tarapacky <alex.tarapacky@caseware.com>
2020-06-26 11:25:17 -0400

    regions

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -38,35 +38,26 @@
 
 <div class="region__dropdown dropdown">
     @php
-    $languages = array("en"=>"EN", "fr"=>"FR", "es"=>"ES", "nl"=>"NL","cn"=>"CN", "de"=>"DE");
+    $regions = array("ca"=>"CA", "us"=>"US");
     $segments = Request::segments();
     // dd(Request::segments()); 
-    $segments[1] = 'ca';
+    $segments[0] = 'ca';
     $caLink = '/' . implode('/', $segments);
-    $segments[1] = 'fr';
-    $frLink = '/' . implode('/', $segments);
-    $segments[1] = 'es';
-    $esLink = '/' . implode('/', $segments);
-    $segments[1] = 'nl';
-    $nlLink = '/' . implode('/', $segments);
-    $segments[1] = 'cn';
-    $cnLink = '/' . implode('/', $segments);
-    $segments[1] = 'de';
-    $deLink = '/' . implode('/', $segments);
+    $segments[0] = 'us';
+    $usLink = '/' . implode('/', $segments);
+
     if(strpos(Request::url(), '/search/') == true || strpos(Request::url(), '/se-search/') == true) {
-        $currentLanguage = $languages[Request::segments()[5]];
+        $currentRegion = $regions[Request::segments()[3]];
     }
     else {
-        $currentLanguage = $languages[Request::segments()[1]];
+        $currentRegion = $regions[Request::segments()[0]];
     }
     @endphp
-    <a href="#"><i class="fas fa-globe-americas"></i> {{$currentLanguage}} <i class="fas fa-angle-down"></i></a>
+    <a href="#">
+        <i class="fas fa-globe-americas"></i> {{$currentRegion}} <i class="fas fa-angle-down"></i>
+    </a>
     <div class="dropdown-content">
-        <a href="{{ $caLink }}">HELLO</a>
-        <a href="{{ $frLink }}">French</a>
-        <a href="{{ $esLink }}">Spanish</a>
-        <a href="{{ $nlLink }}">Dutch</a>
-        <a href="{{ $cnLink }}">Chinese</a>
-        <a href="{{ $deLink }}">German</a>
+        <a href="{{ $caLink }}">Canada</a>
+        <a href="{{ $usLink }}">US</a>
     </div>
 </div>
