ff169a3124dd840f64c4f7c8acfa4fbcbe845fa1
Alex Tarapacky <alex.tarapacky@caseware.com>
2020-06-26 11:11:07 -0400

    regional header

--- resources/views/partials/header-ghost.blade.php
+++ resources/views/partials/header-ghost.blade.php
@@ -1,11 +1,20 @@
+@php 
+$region = Route::current()->parameters()["region"];
+$lang = Route::current()->parameters()["lang"];
+@endphp
+
 <header class="header header--ghost">
     <div class="container header__container">
         {{-- <div class="row"> --}}
         <div class="header__search-wrap">
             <div class="header__nav-wrap">
-              <a href="/"><img class="header__logo" src="/img/CaseWare_logo_RGB_horz_White.png" alt="CaseWare logo"></a>
+                <a href="/{{$region}}/{{$lang}}">
+                    <img class="header__logo" src="/img/CaseWare_logo_RGB_horz_White.png" alt="CaseWare logo">
+                </a>
+                @if(strpos(Request::url(), '/SE-Authoring/') == false)
+                    @include('partials.nav')
+                @endif
             </div>
-            
         </div>
     </div>
     <div class="container">
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,5 +1,5 @@
 
-<div class="language__dropdown">
+<div class="language__dropdown dropdown">
     @php
     $languages = array("en"=>"EN", "fr"=>"FR", "es"=>"ES", "nl"=>"NL","cn"=>"CN", "de"=>"DE");
     $segments = Request::segments();
@@ -26,6 +26,43 @@
     <a href="#"><i class="fas fa-globe-americas"></i> {{$currentLanguage}} <i class="fas fa-angle-down"></i></a>
     <div class="dropdown-content">
         <a href="{{ $enLink }}">English</a>
+        <a href="{{ $frLink }}">French</a>
+        <a href="{{ $esLink }}">Spanish</a>
+        <a href="{{ $nlLink }}">Dutch</a>
+        <a href="{{ $cnLink }}">Chinese</a>
+        <a href="{{ $deLink }}">German</a>
+    </div>
+</div>
+
+
+
+<div class="region__dropdown dropdown">
+    @php
+    $languages = array("en"=>"EN", "fr"=>"FR", "es"=>"ES", "nl"=>"NL","cn"=>"CN", "de"=>"DE");
+    $segments = Request::segments();
+    // dd(Request::segments()); 
+    $segments[1] = 'ca';
+    $caLink = '/' . implode('/', $segments);
+    $segments[1] = 'fr';
+    $frLink = '/' . implode('/', $segments);
+    $segments[1] = 'es';
+    $esLink = '/' . implode('/', $segments);
+    $segments[1] = 'nl';
+    $nlLink = '/' . implode('/', $segments);
+    $segments[1] = 'cn';
+    $cnLink = '/' . implode('/', $segments);
+    $segments[1] = 'de';
+    $deLink = '/' . implode('/', $segments);
+    if(strpos(Request::url(), '/search/') == true || strpos(Request::url(), '/se-search/') == true) {
+        $currentLanguage = $languages[Request::segments()[5]];
+    }
+    else {
+        $currentLanguage = $languages[Request::segments()[1]];
+    }
+    @endphp
+    <a href="#"><i class="fas fa-globe-americas"></i> {{$currentLanguage}} <i class="fas fa-angle-down"></i></a>
+    <div class="dropdown-content">
+        <a href="{{ $caLink }}">HELLO</a>
         <a href="{{ $frLink }}">French</a>
         <a href="{{ $esLink }}">Spanish</a>
         <a href="{{ $nlLink }}">Dutch</a>
