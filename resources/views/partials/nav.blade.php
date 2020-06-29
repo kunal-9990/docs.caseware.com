29dbee8c8b45be8c5b7b8378d4b3c6d8c71af2c9
Matthew Chiang <matthew.chiang@caseware.com>
2020-06-29 16:17:58 -0400

    region/lang nav cleanup

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,65 +1,26 @@
-
-<div class="language__dropdown dropdown">
-    @php
-    $languages = array("en"=>"EN", "fr"=>"FR", "es"=>"ES", "nl"=>"NL","cn"=>"CN", "de"=>"DE");
-    $segments = Request::segments();
-    // dd(Request::segments()); 
-    $segments[1] = 'en';
-    $enLink = '/' . implode('/', $segments);
-    $segments[1] = 'fr';
-    $frLink = '/' . implode('/', $segments);
-    $segments[1] = 'es';
-    $esLink = '/' . implode('/', $segments);
-    $segments[1] = 'nl';
-    $nlLink = '/' . implode('/', $segments);
-    $segments[1] = 'cn';
-    $cnLink = '/' . implode('/', $segments);
-    $segments[1] = 'de';
-    $deLink = '/' . implode('/', $segments);
-    if(strpos(Request::url(), '/search/') == true || strpos(Request::url(), '/se-search/') == true) {
-        $currentLanguage = $languages[Request::segments()[4]];
-    }
-    else {
-        // $currentLanguage = $languages[Request::segments()[1]];
-        isset(Route::current()->parameters()["lang"]) ? $lang = Route::current()->parameters()["lang"] : $lang = 'Language';
-    }
-    @endphp
-    <a href="#"><i class="fas fa-globe-americas"></i> {{$lang}} <i class="fas fa-angle-down"></i></a>
-    <div class="dropdown-content">
-        <a href="{{ str_replace('/'.$lang.'/','en', Request::path()) }}">English</a>
-        <a href="{{ $frLink }}">French</a>
-        <a href="{{ $esLink }}">Spanish</a>
-        <a href="{{ $nlLink }}">Dutch</a>
-        <a href="{{ $cnLink }}">Chinese</a>
-        <a href="{{ $deLink }}">German</a>
+@php
+    isset(Route::current()->parameters()["lang"]) ? $lang = Route::current()->parameters()["lang"] : $lang = '';
+    isset(Route::current()->parameters()["region"]) ? $region = Route::current()->parameters()["region"] : $region = '';
+@endphp
+@if(!empty($lang))
+    <div class="language__dropdown dropdown">
+        <a href="#"><i class="fas fa-globe-americas"></i> {{strtoupper($lang)}} <i class="fas fa-angle-down"></i></a>
+        <div class="dropdown-content">
+            <a href="/{{ str_replace('/'.$lang ,'/en', Request::path()) }}">English</a>
+            <a href="/{{ str_replace('/'.$lang ,'/fr', Request::path()) }}">French</a>
+            <a href="/{{ str_replace('/'.$lang ,'/es', Request::path()) }}">Spanish</a>
+            <a href="/{{ str_replace('/'.$lang ,'/nl', Request::path()) }}">Dutch</a>
+            <a href="/{{ str_replace('/'.$lang ,'/cn', Request::path()) }}">Chinese</a>
+            <a href="/{{ str_replace('/'.$lang ,'/de', Request::path()) }}">German</a>
+        </div>
     </div>
-</div>
-
-
-
-<div class="region__dropdown dropdown">
-    @php
-    $regions = array("ca"=>"CA", "us"=>"US");
-    $segments = Request::segments();
-    // dd(Request::segments()); 
-    $segments[0] = 'ca';
-    $caLink = '/' . implode('/', $segments);
-    $segments[0] = 'us';
-    $usLink = '/' . implode('/', $segments);
-
-    if(strpos(Request::url(), '/search/') == true || strpos(Request::url(), '/se-search/') == true) {
-        $currentRegion = $regions[Request::segments()[3]];
-    }
-    else {
-        // $currentRegion = $regions[Request::segments()[0]];
-        isset(Route::current()->parameters()["region"]) ? $region = Route::current()->parameters()["region"] : $region = 'Region';
-    }
-    @endphp
-    <a href="#">
-        <i class="fas fa-globe-americas"></i> {{$region}} <i class="fas fa-angle-down"></i>
-    </a>
-    <div class="dropdown-content">
-        <a href="{{ $caLink }}">Canada</a>
-        <a href="{{ $usLink }}">US</a>
+@endif
+@if(!empty($lang))
+    <div class="region__dropdown dropdown">
+        <a href="#"><i class="fas fa-globe-americas"></i> {{strtoupper($region)}} <i class="fas fa-angle-down"></i></a>
+        <div class="dropdown-content">
+            <a href="/{{ str_replace($region.'/' , 'ca/', Request::path()) }}">Canada</a>
+            <a href="/{{ str_replace($region.'/' , 'us/', Request::path()) }}">US</a>
+        </div>
     </div>
-</div>
+@endif
