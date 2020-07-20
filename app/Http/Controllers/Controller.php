3c63cbba89ba72cd0ed392b1a422cd4ad1e46386
Matthew Chiang <matthew.chiang@caseware.com>
2020-07-20 17:01:58 -0400

    reverting fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -27,10 +27,10 @@ class Controller extends BaseController
 
         //Grab the menus
         View::share('header', $cms->menu('header')->get('results')->items);
-        //Grab the menus
-        View::share('header', $cms->menu('header')->get('results')->items);
-        $footerRegion =  isset($request->route()->parameters['region']) ? $request->route()->parameters['region'] : 'int';
-        View::share('footer', $cms->menu('Footer-'.$footerRegion)->get('results')->items);
+        View::share('footer-products', $cms->menu('footer-products')->get('results')->items);
+        View::share('footer-about', $cms->menu('footer-about')->get('results')->items);
+        View::share('footer-resources', $cms->menu('footer-resources')->get('results')->items);
+        View::share('footer', $cms->menu('footer')->get('results')->items);
  
 
         //Build a breadcrumb array
