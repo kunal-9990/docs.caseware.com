cff933468a7b2851e7890923b3662da36e6d3b03
Matthew Chiang <matthew.chiang@caseware.com>
2020-10-30 17:28:31 -0400

    adding optinmonster script

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -38,7 +38,10 @@
         @include('partials.cookie-consent')
         @include('partials.footer')
         
-
+    <!-- This site is converting visitors into subscribers and customers with OptinMonster - https://optinmonster.com -->
+    <script type="text/javascript" src="https://a.omappapi.com/app/js/api.min.js" data-account="87970" data-user="78239"
+        async></script>
+    <!-- / https://optinmonster.com -->
     <script src="{{ mix('/js/mk2/app.js') }}"></script>
     <script src="{{ mix('/js/mk4/app.js') }}"></script>
 </body>
