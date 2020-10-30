cd295b89740d03104817b55bc28df074662ef729
Matthew Chiang <matthew.chiang@caseware.com>
2020-10-30 16:16:33 -0400

    adding optin monster script

--- resources/views/partials/footer-includes.blade.php
+++ resources/views/partials/footer-includes.blade.php
@@ -11,7 +11,10 @@ isset(Route::current()->parameters()["region"]) ? $region = Route::current()->pa
     </script>
 @endif
 <!-- end olark code -->
-
+<!-- This site is converting visitors into subscribers and customers with OptinMonster - https://optinmonster.com -->
+<script type="text/javascript" src="https://a.omappapi.com/app/js/api.min.js" data-account="87970" data-user="78239"
+    async></script>
+<!-- / https://optinmonster.com -->
 <script src="https://cdn.polyfill.io/v2/polyfill.min.js"></script>
 <script src="{{ mix('/js/mk2/app.js') }}"></script>
 <script src="{{ mix('/js/mk4/app.js') }}"></script>
