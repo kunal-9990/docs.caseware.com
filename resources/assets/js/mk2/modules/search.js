deaa246e85a222a2fc10bd26a3580d71e7e20cf2
Matthew Chiang <matthew.chiang@caseware.com>
2021-01-15 13:22:01 -0500

    prevent indexing deprecated topics

--- resources/assets/js/mk2/modules/search.js
+++ resources/assets/js/mk2/modules/search.js
@@ -1,13 +1,13 @@
 // /* eslint-disable */
-module.exports = () => {
-    function getParameterByName(name, url) {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return "";
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
-    }
-    const search = getParameterByName('search', window.location.href);
-    window.location.hash = 'search-' + search;
-};
+// module.exports = () => {
+//     function getParameterByName(name, url) {
+//         if (!url) url = window.location.href;
+//         name = name.replace(/[\[\]]/g, "\\$&");
+//         var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),results = regex.exec(url);
+//         if (!results) return null;
+//         if (!results[2]) return "";
+//         return decodeURIComponent(results[2].replace(/\+/g, " "));
+//     }
+//     const search = getParameterByName('search', window.location.href);
+//     window.location.hash = 'search-' + search;
+// };
