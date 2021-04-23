5c0cec1d75d1ff7847efda29c9be9a934b1e9d86
Matthew Chiang <matthew.chiang@caseware.com>
2021-04-23 12:14:02 -0400

    adding olark on topic pages

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -35,7 +35,18 @@
 	  }
 	}
 </script>
-
+@php
+isset(Route::current()->parameters()["lang"]) ? $lang = Route::current()->parameters()["lang"] : $lang = '';
+isset(Route::current()->parameters()["region"]) ? $region = Route::current()->parameters()["region"] : $region = '';
+@endphp
+@if($lang == 'en' && ($region !== 'int' || $region == ""))
+<!-- begin olark code -->
+<script type="text/javascript" async>
+	;(function(o,l,a,r,k,y){if(o.olark)return; r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0]; y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r); y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)}; y.extend=function(i,j){y("extend",i,j)}; y.identify=function(i){y("identify",k.i=i)}; y.configure=function(i,j){y("configure",i,j);k.c[i]=j}; k=y._={s:[],t:[+new Date],c:{},l:a}; })(window,document,"static.olark.com/jsclient/loader.js");
+                /* custom configuration goes here (www.olark.com/documentation) */
+                olark.identify('4439-775-10-8635');
+</script>
+@endif
 
 {{-- Tell Google about localized versions of our pages --}}
 @php
