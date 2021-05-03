739f160d697db2cecabcf282cdd8913011696267
Matthew Chiang <matthew.chiang@caseware.com>
2021-05-03 15:34:31 -0400

    removing cms-based whatsnew pages

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -140,48 +140,7 @@ class PageController extends Controller
         //first check if the topic is a what's new page. 
         //if so - get the content from the cms and return What's New template
         //assume the cms permalink for all WN pages is: "whats-new-product-version"
-        if($subcategory == "Whats-New"){
-            $page = $this->cms->get_custom_post_by_name($lang, 'whats-new', str_replace(".htm", "", $topic));
-            //if the slug is not present in the cms, try displaying the flare-based WN page:
-            if(empty($page["totalPages"])){
-                if(!endsWith($topic,".htm")){
-                    $topic .= ".htm";
-                } 
 
-                // otherwise, get topic content from flare build.
-                $noHeader = true;
-                $product =  strtolower($product);
-
-                try {
-                    $dom = HtmlDomParser::str_get_html(file_get_contents( env('PATH_TO_PUBLIC').'documentation_files/'.$year."/".$product."/".$version."/"."Content/".$lang."/".$category."/".$subcategory."/".$topic ));
-                    $doNotTranslate = true;
-                } catch (Exception $e) {
-                    try {
-                        $dom = HtmlDomParser::str_get_html(file_get_contents( env('PATH_TO_PUBLIC').'documentation_files/'.$year."/".$product."/".$version."/"."Content/en/".$category."/".$subcategory."/".$topic ));
-                        } catch (Exception $e) {
-                            return response()->view('errors.404');
-                        }
-                }
-
-                $maincontentarea = $dom->find('body', 0);
-                $htmlElement = $dom->find('html', 0);
-                (isset($htmlElement->attr['data-mc-conditions'])) ? $exclusiveTo = $htmlElement->attr['data-mc-conditions'] : $exclusiveTo = '' ;
-                $recent = getRecentlyViewed();
-                $title = strip_tags($dom->find('h1', 0));
-                
-                // TODO - undo comment below
-                // return view('pages.whats-new', compact('maincontentarea', 'recent', 'exclusiveTo','title'));
-                return view('pages.documentation', compact('maincontentarea', 'recent', 'exclusiveTo','title'));
-            }
-
-            $pageContent = $page['results'][0];
-            $voteData = getVoteData($pageContent->acf->product, $pageContent->acf->version);
-            $userVotes = session('user.Votes');
-
-            $topicVersion = substr($topic, strrpos($topic, '-') + 1);
-
-            return view('pages.whats-new', compact('pageContent', 'recent', 'exclusiveTo','title', 'voteData', 'userVotes', 'product', 'version'));
-        }
 
         if(!endsWith($topic,".htm")){
             $topic .= ".htm";
