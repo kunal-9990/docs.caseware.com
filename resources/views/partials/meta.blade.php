762288ed823ff235ba508d09c338673d57eaca38
Matthew Chiang <matthew.chiang@caseware.com>
2021-05-03 10:37:10 -0400

    adding google translate to french homepage

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -97,7 +97,7 @@ isset(Route::current()->parameters()["lang"]) ? $lang = Route::current()->parame
 
 	if(myURL.indexOf("/en/") !== -1){
 	document.cookie="googtrans" + "=" + "/en/en/";
-	}else if(myURL.indexOf("/fr/") !== -1){
+	}else if(myURL.indexOf("/fr/") !== -1 || myURL.indexOf("/ca/fr") !== -1){
 	document.cookie="googtrans" + "=" + "/en/fr/";
 	}else if (myURL.indexOf("/es/") !== -1){
 	document.cookie="googtrans" + "=" + "/en/es/";
