a0ad1595f636dc2107c62fea806eb0a5e15ad138
Matthew Chiang <matthew.chiang@caseware.com>
2021-05-06 10:11:10 -0400

    javascript redirects

--- resources/assets/js/mk4/app.js
+++ resources/assets/js/mk4/app.js
@@ -137,3 +137,6 @@ if (window.location.pathname == "/2020/webapps/31/en/Engagements/Template-and-Au
 if (window.location.pathname == "/2020/webapps/31/en/Explore/Whats-New/whats-new-audit-winter-2020.htm" && window.location.hash == "#Simple-Electronic-Signature") {
   window.location.replace('https://docs.caseware.com/latest/webapps/en/Explore/Whats-New/Release-history-Audit-Winter-2020.htm#Simple-Electronic-Signature')
 }
+if (window.location.pathname == "/2020/webapps/31/en/Engagements/Template-and-Authoring/Manage-the-firm-template.htm" && window.location.hash == "#Update") {
+  window.location.replace('https://docs.caseware.com/2020/webapps/31/en/Engagements/Template-and-Authoring/Update-the-firm-template.htm')
+}
