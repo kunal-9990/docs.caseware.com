6fce85311845889afefdc3675bab197003fafa5b
Matthew Chiang <matthew.chiang@caseware.com>
2021-05-06 19:01:19 -0400

    updating publishing script

--- scripts/build.sh
+++ scripts/build.sh
@@ -135,5 +135,5 @@ sudo chmod -R 777 scripts
 
 sudo rm -R /tmp/docs_content/*
 sudo rm -R /usr/share/nginx/docs/tmp/$4/$3/*
-sudo find /usr/share/nginx/docs/public/documentation_files/$1/$2/$4/Content/$4/ -mindepth 1 -type f -mmin +15 -delete
+sudo find /usr/share/nginx/docs/public/documentation_files/$1/$2/$3/Content/$4/ -mindepth 1 -type f -mmin +15 -delete
 echo 'Done.'
