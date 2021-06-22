f5a9975e958ddfc2e7f9a7fb2239562daacac3b2
Matthew Chiang <matthew.chiang@caseware.com>
2021-06-22 13:01:32 -0400

    regionalizing toc's

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,19 +1,34 @@
 @php
     isset(Route::current()->parameters()["lang"]) ? $lang = Route::current()->parameters()["lang"] : $lang = '';
     isset(Route::current()->parameters()["region"]) ? $region = Route::current()->parameters()["region"] : $region = '';
+    isset(Route::current()->parameters()["version"]) ? $version = Route::current()->parameters()["version"] : $version = '';
     (null !== Request::input('region')) ? $tocregion = Request::input('region') : $tocregion = 'int';
 @endphp
 <div class="nav-container">
-@if(!empty($region))
-    <div class="region__dropdown dropdown">
-        <a href="#"><i class="fas fa-globe-americas"></i> <span class="notranslate">{{strtoupper($region)}}</span> <i class="fas fa-angle-down"></i></a>
+{{-- region is controlled via query string on topic pages and using a route parameter on cms pages. checking the version tells us whether
+we're on a topic page or cms page as only topic pages have the version in the url. display the region dropdown accordingly: --}}
+@if(!empty($version))
+<div class="language__dropdown dropdown">
+        <a href="#"><i class="fas fa-globe-americas"></i> <span class="notranslate">{{strtoupper($tocregion)}}</span> <i
+                class="fas fa-angle-down"></i></a>
+        <div class="dropdown-content">
+            <a href="?region=ca">Canada</a>
+            <a href="?region=us">US</a>
+            <a href="?region=int">International</a>
+            <a href="?region=nl">Netherlands</a>
+        </div>
+</div>
+@else
+<div class="region__dropdown dropdown">
+        <a href="#"><i class="fas fa-globe-americas"></i> <span class="notranslate">{{strtoupper($region)}}</span> <i
+                class="fas fa-angle-down"></i></a>
         <div class="dropdown-content">
             <a href="/{{ str_replace($region.'/' , 'int/', Request::path()) }}">International</a>
             <a href="/{{ str_replace($region.'/' , 'ca/', Request::path()) }}">Canada</a>
             <a href="/{{ str_replace($region.'/' , 'us/', Request::path()) }}"><span class="notranslate">US</span></a>
             <a href="/{{ str_replace($region.'/' , 'nl/', Request::path()) }}"><span class="notranslate">NL</span></a>
         </div>
-    </div>
+</div>
 @endif
 @if(!empty($lang))
     <div class="language__dropdown dropdown">
@@ -25,17 +40,6 @@
             <a href="/{{ str_replace('/'.$lang ,'/nl', Request::path()) }}">Dutch</a>
             <a href="/{{ str_replace('/'.$lang ,'/cn', Request::path()) }}">Chinese</a>
             <a href="/{{ str_replace('/'.$lang ,'/de', Request::path()) }}">German</a>
-        </div>
-    </div>
-@endif
-@if(!empty($lang))
-    <div class="language__dropdown dropdown">
-        <a href="#"><i class="fas fa-globe-americas"></i> <span class="notranslate">{{strtoupper($tocregion)}}</span> <i class="fas fa-angle-down"></i></a>
-        <div class="dropdown-content">
-            <a href="?region=ca">Canada</a>
-            <a href="?region=us">US</a>
-            <a href="?region=int">International</a>
-            <a href="?region=nl">Netherlands</a>
         </div>
     </div>
 @endif
