3e8173bdd3d9a49cf73aa91c8cd1416ac2f09292
Matthew Chiang <matthew.chiang@caseware.com>
2021-11-11 10:24:26 -0500

    updating build script so old topic files are removed before new ones are added during publishing process

--- scripts/build.sh
+++ scripts/build.sh
@@ -29,6 +29,8 @@ sudo chmod -R 777 /usr/share/nginx/docs
 
 echo 'Copying new content into place...'
 # mkdir -p public/documentation_files/$1/$2/$3/Content/$4
+mv public/documentation_files/$1/$2/$3/Content/$4 /tmp
+
 cp -R tmp/$4/$3/* public/documentation_files/$1/$2/$3/Content/$4
 cd public/documentation_files/$1/$2/$3/Content/$4
 
