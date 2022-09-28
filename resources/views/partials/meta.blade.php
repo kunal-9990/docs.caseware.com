cd1ae8ac8b1518869e12f3a16ff7a57505ee1d46
mansi-bhavsar <mansi.bhavsar@caseware.com>
2022-09-28 15:37:46 -0400

    added GTM code to meta

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -78,6 +78,15 @@ isset(Route::current()->parameters()["lang"]) ? $lang = Route::current()->parame
 		/* ]]> */</script>
 	@endif
 	{{-- google analytics end--}}
+
+	
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-KXKK445');</script>
+	<!-- End Google Tag Manager -->
 @endif
 
 @if(strpos(URL::current(), '/TranslatedDocs/') === false)
