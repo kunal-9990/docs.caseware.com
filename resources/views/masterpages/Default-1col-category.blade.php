900bbc651b22de021255a66751920b9a61184d5a
mansi-bhavsar <mansi.bhavsar@caseware.com>
2022-09-29 10:58:26 -0400

    old codes removed

--- resources/views/masterpages/Default-1col-category.blade.php
+++ resources/views/masterpages/Default-1col-category.blade.php
@@ -21,30 +21,7 @@
 			<link href="/assets/stylesheets/style.css" rel="stylesheet" type="text/css" />
 			<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
 		<script>/*]]>*/</script>
-        <!--Global Tracking ID - DO NOT TOUCH-->
-        <script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-			ga('create', 'UA-97702818-1', 'auto', 'Global');
-			ga('Global.send', 'pageview', { 'page': location.pathname + location.search + location.hash});
-
-		</script>
-        <!--Global Tracking ID - End-->
-        <!--WebApps Tracking ID-->
-        <script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-			ga('create', 'UA-97702818-2', 'auto', 'Cloud');
-			ga('Cloud.send', 'pageview', { 'page': location.pathname + location.search + location.hash});
-
-		</script>
-        <!--WebApps Tracking ID - End-->
+    
         <!-- Google Tag Manager -->
           <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
