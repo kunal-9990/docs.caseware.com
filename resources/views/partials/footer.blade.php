9b4cd642c31899ba7d3ee3efcd323f97f2e92803
Mansi Bhavsar <mansi.bhavsar@caseware.com>
2023-06-27 16:10:15 -0400

    Updated the legal links in footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -32,15 +32,15 @@
                     <span>About</span>
                     <a href="https://www.caseware.com/ca/about" target="_blank" rel="noopener">Who we are</a>
                     <a href="https://www.caseware.com/ca/careers" target="_blank" rel="noopener">Where you fit</a>
-                    <a href="https://www.caseware.com/ca/cloud-security-compliance" target="_blank" rel="noopener">Certifications</a>
+                    <a href="https://www.caseware.com/security-certifications/" target="_blank" rel="noopener">Certifications</a>
                 </div>
                 <div class="col stretch">
                     <span>Legal</span>
                     <!-- <a href="https://www.caseware.com/generic/privacy-policy-caseware-international" target="_blank" rel="noopener">CaseWare International Privacy Policy</a> -->
-                    <a href="https://www.caseware.com/generic/privacy-policy-caseware-cloud" target="_blank" rel="noopener">Caseware Cloud Privacy Policy</a>
-                    <a href="https://www.caseware.com/generic/terms" target="_blank" rel="noopener">Terms of Use</a>
+                    <a href="https://www.caseware.com/privacy-statement/" target="_blank" rel="noopener">Caseware Cloud Privacy Policy</a>
+                    <a href="https://www.caseware.com/terms-of-use/" target="_blank" rel="noopener">Terms of Use</a>
                     <a href="https://docs.caseware.com/latest/webapps/{{ $lang }}/Setup/Licenses/CaseWare-Cloud-Services-Agreement.htm" rel="noopener">Cloud Services Agreement</a>
-                    <a href="https://www.caseware.com/support/cloud/service-level-agreement" target="_blank" rel="noopener">Cloud Service Level Agreement</a>
+                    <a href="https://docs.caseware.com/latest/webapps/en/Setup/Licenses/CaseWare-Cloud-Services-Agreement.htm?region=int" target="_blank" rel="noopener">Cloud Service Level Agreement</a>
                 </div>
                 <div class="col">
                     <span>Resources</span>
