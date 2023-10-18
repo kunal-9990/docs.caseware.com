b2d983b325f32e8694cb3a958a55467b46785035
mansi-bhavsar <mansi.bhavsar@caseware.com>
2023-10-18 13:46:35 -0400

    ISERV-1570-added string translations

---
+++ resources/lang/de/strings.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+
+  //MyCaseWare
+  'in_this_article' => 'In diesem Beitrag',
+  'recently_viewed' => 'Zuletzt angesehen',
+  'was_this_helpful' => 'War dieser Beitrag hilfreich?',
+  'yes' => 'Ja',
+  'no' => 'Nein',
+  'search' => 'Suchen'
+];
