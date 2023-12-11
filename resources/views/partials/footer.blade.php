da727d871da807a790e64182cc64881209c68847
mansi-bhavsar <mansi.bhavsar@caseware.com>
2023-12-11 15:36:13 -0500

    added footer translations

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -16,10 +16,10 @@
                 </a>
             </div>
             <div class="col col-sm-6 col--sm-full footer__social-media">
-                <a href="https://www.linkedin.com/groups/1935174/profile"><i class="fab fa-linkedin"></i></a>
-                <a href="http://www.facebook.com/pages/CaseWare-International-Inc/54418196590?ref=ts"><i class="fab fa-facebook-square"></i></a>
-                <a href="https://twitter.com/CaseWare"><i class="fab fa-twitter-square"></i></a>
-                <a href="http://www.youtube.com/user/casewarevids?feature=results_main"><i class="fab fa-youtube-square"></i></a>
+                {!! __(strings.social_media_1) !!}
+                {!! __(strings.social_media_2) !!}
+                {!! __(strings.social_media_3) !!}
+                {!! __(strings.social_media_4) !!}
             </div>
         </div>
         <div class="row footer__links">
@@ -29,30 +29,30 @@
            
 
                 <div class="col">
-                    <span>{{ __('strings.footer_link_about') }}</span>
-                    <a href="https://www.caseware.com/ca/about" target="_blank" rel="noopener">{{ __('strings.footer_link_who_we_are') }}</a>
-                    <a href="https://www.caseware.com/ca/careers" target="_blank" rel="noopener">{{ __('strings.footer_link_where_you_fit') }}</a>
-                    <a href="https://www.caseware.com/security-certifications/" target="_blank" rel="noopener">{{ __('strings.footer_link_certifications') }}</a>
+                    <span>{{ __('strings.footer_text_about') }}</span>
+                    <a href="{{ __('strings.footer_href_who_we_are') }}" target="_blank" rel="noopener">{{ __('strings.footer_text_who_we_are') }}</a>
+                    <a href="{{ __('strings.footer_href_where_you_fit') }}" target="_blank" rel="noopener">{{ __('strings.footer_text_where_you_fit') }}</a>
+                    <a href="{{ __('strings.footer_href_certifications') }}" target="_blank" rel="noopener">{{ __('strings.footer_text_certifications') }}</a>
                 </div>
                 <div class="col stretch">
-                    <span>{{ __('strings.footer_link_legal') }}</span>
+                    <span>{{ __('strings.footer_text_legal') }}</span>
                     <!-- <a href="https://www.caseware.com/generic/privacy-policy-caseware-international" target="_blank" rel="noopener">CaseWare International Privacy Policy</a> -->
-                    <a href="https://www.caseware.com/privacy-statement/" target="_blank" rel="noopener">{{ __('strings.footer_link_caseware_cloud_privacy_policy') }}</a>
-                    <a href="https://www.caseware.com/terms-of-use/" target="_blank" rel="noopener">{{ __('strings.footer_link_terms_of_use') }}</a>
-                    <a href="https://docs.caseware.com/latest/webapps/{{ $lang }}/Setup/Licenses/CaseWare-Cloud-Services-Agreement.htm" rel="noopener">{{ __('strings.footer_link_cloud_services_agreement') }}</a>
-                    <a href="https://docs.caseware.com/latest/webapps/en/Setup/Licenses/CaseWare-Cloud-Services-Agreement.htm?region=int" target="_blank" rel="noopener">{{ __('strings.footer_link_cloud_service_level_agreement') }}</a>
+                    <a href="{{ __('strings.footer_href_caseware_cloud_privacy_policy') }}" target="_blank" rel="noopener">{{ __('strings.footer_text_caseware_cloud_privacy_policy') }}</a>
+                    <a href="{{ __('strings.footer_href_terms_of_use') }}" target="_blank" rel="noopener">{{ __('strings.footer_text_terms_of_use') }}</a>
+                    <a href="{{ __('strings.footer_href_cloud_services_agreement') }}" rel="noopener">{{ __('strings.footer_text_cloud_services_agreement') }}</a>
+                    <a href="{{ __('strings.footer_href_cloud_service_level_agreement') }}" target="_blank" rel="noopener">{{ __('strings.footer_text_cloud_service_level_agreement') }}</a>
                 </div>
                 <div class="col">
-                    <span>{{ __('strings.footer_link_resources') }}</span>
-                    <a href="https://www.caseware.com/ca/support" target="_blank" rel="noopener">{{ __('strings.footer_link_support') }}</a>
-                    <a href="https://www.caseware.com/ca/training" target="_blank" rel="noopener">{{ __('strings.footer_link_training') }}</a>
-                    <a href="https://www.caseware.com/ca/distributors" target="_blank" rel="noopener">{{ __('strings.footer_link_distributors') }}</a>
-                    <a href="https://my.caseware.com/account/login?ReturnUrl=%2F" target="_blank" rel="noopener">{{ __('strings.footer_link_mycaseware') }}</a>
+                    <span>{{ __('strings.footer_text_resources') }}</span>
+                    <a href="{{ __('strings.footer_href_support') }}" target="_blank" rel="noopener">{{ __('strings.footer_text_support') }}</a>
+                    <a href="{{ __('strings.footer_href_training') }}" target="_blank" rel="noopener">{{ __('strings.footer_text_training') }}</a>
+                    <a href="{{ __('strings.footer_href_distributors') }}" target="_blank" rel="noopener">{{ __('strings.footer_text_distributors') }}</a>
+                    <a href="{{ __('strings.footer_href_mycaseware') }}" target="_blank" rel="noopener">{{ __('strings.footer_text_mycaseware') }}</a>
                 </div>
                 <div class="col stretch contact">
-                    <span>{{ __('strings.footer_link_contact') }}</span>
+                    <span>{{ __('strings.footer_text_contact') }}</span>
                     <div>
-                        {{ __('strings.footer_address') }}
+                        {!! __('strings.footer_address') !!}
                         <a href="tel:14168679504">{{ __('strings.footer_telephone') }}</a>
                         <a href="tel:14168671906">{{ __('strings.footer_fax') }}</a>
                     </div>
