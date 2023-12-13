8c4b70e19711327d87f0de62bb01133da444559a
mansi-bhavsar <mansi.bhavsar@caseware.com>
2023-12-13 14:38:50 -0500

    trying new lang/region dropdown

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,6 +3,28 @@ isset(Route::current()->parameters()["lang"]) ? $lang = Route::current()->parame
 isset(Route::current()->parameters()["region"]) ? $region = Route::current()->parameters()["region"] : $region = '';
 isset(Route::current()->parameters()["version"]) ? $version = Route::current()->parameters()["version"] : $version = '';
 (null !== Request::input('region')) ? $tocregion = Request::input('region') : $tocregion = 'int';
+
+//array to identify region & language together for dropdown
+$region_array = [
+    "ca": "Canada",
+    "us": "US",
+    "int": "International"
+];
+$lang_array = [
+    "en": "English",
+    "fr": "Français",
+    "nl": "Nederlands"
+];
+$selected_option = ($lang_array[$lang])." (".$region_array[$tocregion].")";
+
+//list of URLs for the dropdown options 
+$ca_en = Request::path();
+$us_en = Request::path();
+$int_en = Request::path();
+$ca_fr = Request::path();
+$int_nl = Request::path();
+$int_de = Request::path();
+
 @endphp
 <div class="nav-container">
     {{-- region is controlled via query string on topic pages and controlled using a route parameter on cms pages. checking the version tells us whether
@@ -10,30 +32,80 @@ we're on a topic page or cms page as only topic pages have the version in the ur
 
     {{-- on topic pages --}}
     @if(!empty($version))
-    <div class="language__dropdown dropdown">
-        <a href="#"><i class="fas fa-globe-americas"></i> <span class="notranslate">{{strtoupper($tocregion)}}</span> <i
-                class="fas fa-angle-down"></i></a>
+
+        $ca_en = str_replace('/'.$lang ,'/en', $ca_en);
+        $ca_en.="?region=ca";
+        $us_en = str_replace('/'.$lang ,'/en', $us_en);
+        $us_en.="?region=us";
+        $int_en = str_replace('/'.$lang ,'/en', $int_en);
+        $int_en.="?region=int";
+        $ca_fr = str_replace('/'.$lang ,'/fr', $ca_fr);
+        $ca_fr.="?region=ca";
+        $int_nl = str_replace('/'.$lang ,'/nl', $int_nl);
+        $int_nl.="?region=int";
+        $int_de = str_replace('/'.$lang ,'/de', $int_de);
+        $int_de.="?region=int";
+
+        <div class="language__dropdown dropdown">
+            <a href="#"><i class="fas fa-globe-americas"></i> <span class="notranslate">{{$selected_option}}</span> <i class="fas fa-angle-down"></i></a>
+            <div class="dropdown-content">
+                <a href="/{{ $ca_en }}"> English (Canada) </a>
+                <a href="/{{ $us_en }}"> English (US) </a>
+                <a href="/{{ $int_en }}"> English (International) </a>
+                <a href="/{{ $ca_fr }}"> Français (Canada) </a>
+                <a href="/{{ $int_nl }}"> Nederlands (Nederland) </a>
+                <a href="/{{ $int_de }}"> Deutsch (Deutschland) </a>
+            </div>
+        </div>
+
+    <!-- <div class="language__dropdown dropdown">
+        <a href="#"><i class="fas fa-globe-americas"></i> <span class="notranslate">{{strtoupper($tocregion)}}</span> <i class="fas fa-angle-down"></i></a>
         <div class="dropdown-content">
             <a href="?region=ca">Canada</a>
             <a href="?region=us">US</a>
             <a href="?region=int">International</a>
         </div>
-    </div>
+    </div> -->
     {{-- on cms pages --}}
     @else
-    <div class="region__dropdown dropdown">
-        <a href="#"><i class="fas fa-globe-americas"></i> <span class="notranslate">{{strtoupper($region)}}</span> <i
-                class="fas fa-angle-down"></i></a>
+    
+        $ca_en = str_replace('/'.$lang ,'/en', $ca_en);
+        $ca_en = str_replace($region.'/' , 'ca/', $ca_en);
+        $us_en = str_replace('/'.$lang ,'/en', $us_en);
+        $us_en = str_replace($region.'/' , 'us/', $us_en);
+        $int_en = str_replace('/'.$lang ,'/en', $int_en);
+        $int_en = str_replace($region.'/' , 'int/', $int_en);
+        $ca_fr = str_replace('/'.$lang ,'/fr', $ca_fr);
+        $ca_fr = str_replace($region.'/' , 'ca/', $ca_fr);
+        $int_nl = str_replace('/'.$lang ,'/nl', $int_nl);
+        $int_nl = str_replace($region.'/' , 'int/', $int_nl);
+        $int_de = str_replace('/'.$lang ,'/de', $int_de);
+        $int_de = str_replace($region.'/' , 'int/', $int_de);
+
+        <div class="language__dropdown dropdown">
+            <a href="#"><i class="fas fa-globe-americas"></i> <span class="notranslate">{{$selected_option}}</span> <i class="fas fa-angle-down"></i></a>
+            <div class="dropdown-content">
+                <a href="/{{ $ca_en }}"> English (Canada) </a>
+                <a href="/{{ $us_en }}"> English (US) </a>
+                <a href="/{{ $int_en }}"> English (International) </a>
+                <a href="/{{ $ca_fr }}"> Français (Canada) </a>
+                <a href="/{{ $int_nl }}"> Nederlands (Nederland) </a>
+                <a href="/{{ $int_de }}"> Deutsch (Deutschland) </a>
+            </div>
+        </div>
+
+    <!-- <div class="region__dropdown dropdown">
+        <a href="#"><i class="fas fa-globe-americas"></i> <span class="notranslate">{{strtoupper($region)}}</span> <i class="fas fa-angle-down"></i></a>
         <div class="dropdown-content">
             <a href="/{{ str_replace($region.'/' , 'int/', Request::path()) }}">International</a>
             <a href="/{{ str_replace($region.'/' , 'ca/', Request::path()) }}">Canada</a>
             <a href="/{{ str_replace($region.'/' , 'us/', Request::path()) }}"><span class="notranslate">US</span></a>
             <a href="/{{ str_replace($region.'/' , 'nl/', Request::path()) }}"><span class="notranslate">NL</span></a>
         </div>
-    </div>
+    </div> -->
     @endif
-    @if(!empty($lang))
-    <div class="language__dropdown dropdown">
+    <!-- @if(!empty($lang)) -->
+    <!-- <div class="language__dropdown dropdown">
         <a href="#"><i class="fas fa-language"></i> <span class="notranslate">{{strtoupper($lang)}}</span> <i
                 class="fas fa-angle-down"></i></a>
         <div class="dropdown-content">
@@ -44,6 +116,6 @@ we're on a topic page or cms page as only topic pages have the version in the ur
             <a href="/{{ str_replace('/'.$lang ,'/cn', Request::path()) }}">Chinese</a>
             <a href="/{{ str_replace('/'.$lang ,'/de', Request::path()) }}">German</a>
         </div>
-    </div>
-    @endif
+    </div> -->
+    <!-- @endif -->
 </div>
