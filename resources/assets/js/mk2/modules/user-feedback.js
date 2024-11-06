8ec765b5de19f7e800d6ae6f2412ec1e0b8dd300
kunal-9990 <kunal.pandey@caseware.com>
2024-11-06 13:30:16 +0530

    Gtag Changes

--- resources/assets/js/mk2/modules/user-feedback.js
+++ resources/assets/js/mk2/modules/user-feedback.js
@@ -5,30 +5,46 @@ module.exports = () => {
     const NOT_HELPFUL_DROPDOWN = document.querySelector('.was-this-helpful-no-reason-dropdown');
     const NOT_HELPFUL_REASON = document.querySelectorAll('input[name=was-this-helpful-no-reason]');
 
-    // variable for the ga script object
+    // variable for the feedback
     let FEEDBACK = null; // eslint-disable-line
 
+    // Track when the information was helpful
     function informationWasHelpful() {
-        // build the ga object here
         FEEDBACK = 'info was helpful';
+        sendFeedbackEvent('helpful', 'User Feedback', window.location.href);
     }
 
+    // Track when the information was not helpful and capture the reason
     function informationWasNotHelpful(reason) {
-        // build the ga object here
         FEEDBACK = `is not helpful: ${reason}`;
+        sendFeedbackEvent('not_helpful', reason, window.location.href);
         hideNoReasonDropdown();
     }
 
+    // Hide the reason dropdown for 'not helpful'
     function hideNoReasonDropdown() {
         NOT_HELPFUL_DROPDOWN.classList.remove('is-expanded');
     }
 
+    // Function to send the event to GA4
+    function sendFeedbackEvent(feedbackType, feedbackValue, pageUrl) {
+        gtag('event', 'user_feedback', {
+            'event_category': 'Feedback',  // Event category
+            'event_label': pageUrl,        // Event label (current page URL)
+            'feedback_type': feedbackType, // Custom parameter for helpful or not helpful
+            'feedback_value': feedbackValue, // Custom parameter for the feedback value or reason
+            'page_location': pageUrl // You can also track the page location separately
+        });
+    }
+
     window.addEventListener('beforeunload', () => {
-        // fire ga script with built object FEEDBACK
-        ga('Global.send', 'event', FEEDBACK, 'User Feedback', window.location.href); // eslint-disable-line
-        ga('Cloud.send', 'event', FEEDBACK, 'User Feedback', window.location.href); // eslint-disable-line
+        // Send the feedback event when the page is unloaded
+        if (FEEDBACK) {
+            sendFeedbackEvent(FEEDBACK, 'User Feedback', window.location.href);
+        }
     });
 
+    // Attach event listeners for reasons (when user clicks "not helpful" reasons)
     NOT_HELPFUL_REASON.forEach((input) => {
         input.addEventListener('click', (event) => {
             const reason = event.target.getAttribute('value');
@@ -36,16 +52,20 @@ module.exports = () => {
         });
     });
 
+    // Attach event listener for the "No" button to show the dropdown
     NOT_HELPFUL_BTN.addEventListener('click', () => {
         NOT_HELPFUL_DROPDOWN.classList.toggle('is-expanded');
     });
 
-    HELPFUL_REASON_BTN.forEach((btn) => {
+    // Attach event listeners for the feedback buttons ("Yes" or "No")
+    HELPFUL_REASON_BTN.forEach((btn) => { 
         btn.addEventListener('click', () => {
+            // Remove the 'feedback-given' class from all buttons
             HELPFUL_REASON_BTN.forEach((el) => {
                 el.classList.remove('feedback-given');
             });
 
+            // Track whether the feedback is helpful or not and hide dropdown if applicable
             if (btn === WAS_HELPFUL_BTN) {
                 hideNoReasonDropdown();
                 informationWasHelpful();
