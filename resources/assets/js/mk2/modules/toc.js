def163ab206407807e7602af87a3aca233ab144d
kunal-9990 <kunal.pandey@caseware.com>
2024-11-20 15:15:00 +0530

    Updated 7

--- resources/assets/js/mk2/modules/toc.js
+++ resources/assets/js/mk2/modules/toc.js
@@ -63,155 +63,138 @@ module.exports = () => {
         }
         console.log(TOCxml);
 
-    $.ajax({
-        type: "GET",
-        url: TOCxml,
-        success: function (xml) {
-            var ul_main = $('<ul class="toc notranslate">');
-            $(xml).find("TocEntry").each(function () {
-                if ($(this).children().length && $(this).parent().is("CatapultToc")) {
-                    var subCatList = $('<ul class="toc__sub-category-wrap">');
-                    $(this).children().each(function () {
-                        if ($(this).children().length) {
-                            var topicList = $('<ul class="toc__topic-wrap">');
-                            $(this).children().each(function () {
-                                var classes;
-                                if (($(this).attr("conditions"))) {
-                                    producttags = $(this).attr("conditions").replace("Product.", "toc__filters--").toLowerCase() + "-js";
-                                } else {
-                                    producttags = " ";
-                                };
-
-                                if ($(this).attr("Link") && loc.includes($(this).attr("Link").replace(".htm", ""))) {
-                                    topicList.append('<li class="current-page ' + producttags + '"><a href="' + linkPrefix + $(this).attr("Link") + '">' + $(this).attr("Title") + '</a></li>');
-                                } else {
-                                    topicList.append('<li class="' + producttags + '"><a href="' + linkPrefix + $(this).attr("Link") + '">' + $(this).attr("Title") + '</a></li>');
-                                }
-
-                            });
-                            var link;
-                            if ($(this).attr("Link")) {
-                                link = '<a href="' + linkPrefix + $(this).attr("Link") + '">';
-                            } else {
-                                link = '';
-                            }
-                            var li = $('<li class="toc__sub-category"><a class="chevron" href="' + linkPrefix + $(this).attr("Link") + '">' + $(this).attr("Title") + '</a>');
-
-                            subCatList.append(li.append(topicList));
-                        } else {
-                            if ($(this).attr("Link") && loc.includes($(this).attr("Link"))) {
-                                subCatList.append('<li class="current-page toc__sub-category"><a class="chevron" href="' + linkPrefix + $(this).attr("Link") + '">' + $(this).attr("Title") + '</a>');
-                            } else {
-                                subCatList.append('<li class="toc__sub-category"><a class="chevron" href="' + linkPrefix + $(this).attr("Link") + '">' + $(this).attr("Title") + '</a>');
-                            }
-                        }
-                    });
-                    var li = $('<li class="toc__category"><a class="chevron" href="#">' + $(this).attr("Title") + '</a>');
-                    ul_main.append(li.append(subCatList));
-                }
-            });
-
-            $(".toc__container").append(ul_main);
-
-            //expand section of current page
-            $(".current-page").parent().addClass("toc__topic-wrap--is-expanded");
-            $(".current-page").parent().parent().parent().addClass("toc__sub-category-wrap--is-expanded");
-
-
-            $(".current-page").parent().addClass("toc__sub-category-wrap--is-expanded");
-            $(".current-page").parent().parent().parent().addClass("toc__sub-category-wrap--is-expanded");
-
-            ifSiblingElementExists();
-
-            tocExpandToggle();
-
-            CheckExpandedLists();
-
-            if (urlParams.get('region')) {
-                $(".toc__container").find("a").each(function () {
-                    var href = $(this).attr('href');
-
-                    if (href) {
-                        href += (href.match(/\?/) ? '&' : '?') + "region=" + urlParams.get('region');
-                        $(this).attr('href', href);
+        $.ajax({
+            type: "GET",
+            url: TOCxml,
+            success: function (xml) {
+                var ul_main = $('<ul class="toc notranslate">');
+                $(xml).find("TocEntry").each(function () {
+                    if ($(this).children().length && $(this).parent().is("CatapultToc")) {
+                        var subCatList = generateTocTree($(this));
+                        var li = $('<li class="toc__category"><a class="chevron" href="#">' + $(this).attr("Title") + '</a>');
+                        ul_main.append(li.append(subCatList));
                     }
                 });
-            }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            console.log(jqXHR);
-            console.log(textStatus);
-            console.log(errorThrown);
-        }
-    });
-
-    // checks to see where to add class to rotate the chevron for expanded toc lists
-    // TODO: might have to polyfill .closest() for IE`
-    function CheckExpandedLists() {
-        const CURRENT_PAGE = document.querySelectorAll('.current-page') || null;
-        CURRENT_PAGE.forEach(el => {
-            el.closest('.toc__sub-category').classList.add('toc__category--is-open');
-            el.closest('.toc__category').classList.add('toc__category--is-open');
-        })
-
-    }
-
-
-    // if an anchor in the toc doesn't have a ul as a next sibling
-    // remove the chevron
-    function ifSiblingElementExists() {
-        const tocLinks = document.querySelectorAll('.toc__container a');
-        tocLinks.forEach((btn) => {
-            if (btn.nextElementSibling === null) {
-                btn.classList.remove('chevron');
+        
+                $(".toc__container").append(ul_main);
+        
+                // Expand section of current page
+                $(".current-page").parent().addClass("toc__topic-wrap--is-expanded");
+                $(".current-page").parent().parent().parent().addClass("toc__sub-category-wrap--is-expanded");
+        
+                $(".current-page").parent().addClass("toc__sub-category-wrap--is-expanded");
+                $(".current-page").parent().parent().parent().addClass("toc__sub-category-wrap--is-expanded");
+        
+                ifSiblingElementExists();
+                tocExpandToggle();
+                CheckExpandedLists();
+        
+                if (urlParams.get('region')) {
+                    $(".toc__container").find("a").each(function () {
+                        var href = $(this).attr('href');
+                        if (href) {
+                            href += (href.match(/\?/) ? '&' : '?') + "region=" + urlParams.get('region');
+                            $(this).attr('href', href);
+                        }
+                    });
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
             }
         });
-    }
-
-    // toc event listener to expand and collapse navs
-    function tocExpandToggle() {
-        const tocLinks = document.querySelectorAll('.toc__container a');
-
-        tocLinks.forEach((link) => {
-            link.addEventListener('click', (event) => {
-                const nextElementSibling = event.target.nextElementSibling;
-                const thisElParentNode = event.target.parentNode;
-
-                if (link.classList.contains('chevron')) {
-                    event.preventDefault();
-
-                    if (nextElementSibling !== null) {
-                        const nextElSiblingClass = nextElementSibling.classList;
-
-                        if (nextElSiblingClass.contains('toc__sub-category-wrap')) {
-                            if (nextElSiblingClass.contains('toc__sub-category-wrap--is-expanded')) {
-                                nextElSiblingClass.remove('toc__sub-category-wrap--is-expanded');
-                                toggleParentisExpandClass(thisElParentNode);
-                            } else {
-                                nextElSiblingClass.add('toc__sub-category-wrap--is-expanded');
-                                toggleParentisExpandClass(thisElParentNode);
-                            }
-                        } else if (nextElSiblingClass.contains('toc__topic-wrap')) {
-                            if (nextElSiblingClass.contains('toc__topic-wrap--is-expanded')) {
-                                nextElSiblingClass.remove('toc__topic-wrap--is-expanded');
-                                toggleParentisExpandClass(thisElParentNode);
-                            } else {
-                                nextElSiblingClass.add('toc__topic-wrap--is-expanded');
-                                toggleParentisExpandClass(thisElParentNode);
+        
+        // Recursive function to generate TOC tree structure
+        function generateTocTree(entry) {
+            var ul = $('<ul class="toc__sub-category-wrap">');
+            entry.children().each(function () {
+                var subCatLink = $(this).attr("Link") ? linkPrefix + $(this).attr("Link") : '#';
+                var producttags = $(this).attr("conditions") ? $(this).attr("conditions").replace("Product.", "toc__filters--").toLowerCase() + "-js" : '';
+        
+                // Check if there are nested elements
+                if ($(this).children().length) {
+                    var topicList = $('<ul class="toc__topic-wrap">');
+                    $(this).children().each(function () {
+                        var link = $(this).attr("Link") ? linkPrefix + $(this).attr("Link") : '#';
+                        var title = $(this).attr("Title");
+                        var listItem = '<li class="' + producttags + '"><a href="' + link + '">' + title + '</a></li>';
+                        topicList.append(listItem);
+                    });
+        
+                    var li = $('<li class="toc__sub-category"><a class="chevron" href="' + subCatLink + '">' + $(this).attr("Title") + '</a>');
+                    ul.append(li.append(topicList));
+                } else {
+                    var li = $('<li class="toc__sub-category"><a class="chevron" href="' + subCatLink + '">' + $(this).attr("Title") + '</a>');
+                    ul.append(li);
+                }
+            });
+            return ul;
+        }
+        
+        // checks to see where to add class to rotate the chevron for expanded toc lists
+        function CheckExpandedLists() {
+            const CURRENT_PAGE = document.querySelectorAll('.current-page') || null;
+            CURRENT_PAGE.forEach(el => {
+                el.closest('.toc__sub-category').classList.add('toc__category--is-open');
+                el.closest('.toc__category').classList.add('toc__category--is-open');
+            });
+        }
+        
+        // if an anchor in the toc doesn't have a ul as a next sibling
+        // remove the chevron
+        function ifSiblingElementExists() {
+            const tocLinks = document.querySelectorAll('.toc__container a');
+            tocLinks.forEach((btn) => {
+                if (btn.nextElementSibling === null) {
+                    btn.classList.remove('chevron');
+                }
+            });
+        }
+        
+        // toc event listener to expand and collapse navs
+        function tocExpandToggle() {
+            const tocLinks = document.querySelectorAll('.toc__container a');
+            tocLinks.forEach((link) => {
+                link.addEventListener('click', (event) => {
+                    const nextElementSibling = event.target.nextElementSibling;
+                    const thisElParentNode = event.target.parentNode;
+        
+                    if (link.classList.contains('chevron')) {
+                        event.preventDefault();
+                        if (nextElementSibling !== null) {
+                            const nextElSiblingClass = nextElementSibling.classList;
+        
+                            if (nextElSiblingClass.contains('toc__sub-category-wrap')) {
+                                if (nextElSiblingClass.contains('toc__sub-category-wrap--is-expanded')) {
+                                    nextElSiblingClass.remove('toc__sub-category-wrap--is-expanded');
+                                    toggleParentisExpandClass(thisElParentNode);
+                                } else {
+                                    nextElSiblingClass.add('toc__sub-category-wrap--is-expanded');
+                                    toggleParentisExpandClass(thisElParentNode);
+                                }
+                            } else if (nextElSiblingClass.contains('toc__topic-wrap')) {
+                                if (nextElSiblingClass.contains('toc__topic-wrap--is-expanded')) {
+                                    nextElSiblingClass.remove('toc__topic-wrap--is-expanded');
+                                    toggleParentisExpandClass(thisElParentNode);
+                                } else {
+                                    nextElSiblingClass.add('toc__topic-wrap--is-expanded');
+                                    toggleParentisExpandClass(thisElParentNode);
+                                }
                             }
                         }
                     }
-                }
+                });
             });
-        });
-    }
-
-    // toggle class that handles rotating chevron
-    function toggleParentisExpandClass(el) {
-        el.classList.contains('toc__category--is-open') ?
-            el.classList.remove('toc__category--is-open') :
-            el.classList.add('toc__category--is-open')
-    }
+        }
+        
+        // toggle class that handles rotating chevron
+        function toggleParentisExpandClass(el) {
+            el.classList.contains('toc__category--is-open') ?
+                el.classList.remove('toc__category--is-open') :
+                el.classList.add('toc__category--is-open');
+        }
 
 
 };
